d5abffdb8f76662af6ccbc01a46f0dea5b97b352
Abdelhameid Elshaht <elshahat799@gmail.com>
2025-01-18 21:23:36 +0200

    Time: 2 ms (81.47%) | Memory: 44.6 MB (9.23%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cs
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cs
@@ -2,7 +2,6 @@ public class Solution {
      public bool CheckIfExist(int[] arr) {
         HashSet<int> seen = new HashSet<int>();
         foreach (int num in arr) {
-            // Check if num is double of a seen number or half of a seen number
             if (seen.Contains(2 * num) || (num % 2 == 0 && seen.Contains(num / 2))) {
                 return true;
             }
