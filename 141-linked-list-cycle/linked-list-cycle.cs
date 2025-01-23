9be6cc41dfef3f4815c9e84eb0cda00dbe21928a
Abdelhameid Elshaht <elshahat799@gmail.com>
2025-01-23 11:26:42 +0200

    Time: 85 ms (98.69%) | Memory: 49.2 MB (5.74%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        HashSet<ListNode> visitedNodes = new HashSet<ListNode>();
+        ListNode cur = head;
+        if(head == null){
+            return false;
+        }
+        while(cur != null){
+            if(visitedNodes.Contains(cur)){
+                return true;
+            }
+            visitedNodes.Add(cur);
+            cur= cur.next;
+        }
+        return false;
+    }
+}
