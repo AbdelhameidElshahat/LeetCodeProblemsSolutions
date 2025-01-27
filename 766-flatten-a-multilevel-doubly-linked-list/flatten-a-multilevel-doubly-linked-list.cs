eddf691aa18da3b8c10c795896150ce9e91f1539
Abdelhameid Elshaht <elshahat799@gmail.com>
2025-01-27 10:09:23 +0200

    Time: 65 ms (86.82%) | Memory: 41.8 MB (39.53%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cs
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cs
@@ -10,23 +10,23 @@ public class Node {
 
 public class Solution {
     public Node Flatten(Node head) {
-        if(head == null) return null;
-        Stack<Node>  stack = new Stack<Node>();
+        if(head == null )return null;
+        Stack<Node> stack = new Stack<Node>();
         Node cur = head;
         while(cur != null){
-        if(cur.child != null){
-            if(cur.next != null )stack.Push(cur.next);
-            cur.next = cur.child;
-            cur.child.prev = cur;
-            cur.child=null;
+            if(cur.child !=null){
+                if(cur.next != null) stack.Push(cur.next);
+                cur.next = cur.child; 
+                cur.child.prev = cur;
+                cur.child=null;
+            }
+            else if (cur.next ==null &&stack.Count>0) {
+                Node node = stack.Pop();
+                cur.next = node;
+                node.prev = cur ;
+            }
+            cur=cur.next;
         }
-        else if(cur.next ==null && stack.Count > 0 ){
-            Node node = stack.Pop();
-            cur.next =node;
-            node.prev =cur;
-        }
-        cur = cur.next;
-    }
-    return head;
+        return head;
     }
 }
