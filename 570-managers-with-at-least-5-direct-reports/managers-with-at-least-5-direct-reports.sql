953f083fc0980c26bbf390e648f08de30ff31bc8
Abdelhameid Elshaht <elshahat799@gmail.com>
2025-02-12 11:22:50 +0200

    Time: 1449 ms (59.84%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,6 @@
 /* Write your T-SQL query statement below */
-SELECT m.name
-FROM employee m
-JOIN employee e ON m.id = e.managerId
-GROUP BY m.id, m.name
-HAVING COUNT(*) >= 5;
+select name from employee where Id In
+(Select managerId   
+from employee 
+group by managerId   
+Having Count(managerId)>=5)
