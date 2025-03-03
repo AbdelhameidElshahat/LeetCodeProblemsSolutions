89626464939383e2e83c9ed175e39b61ff8c22a4
Abdelhameid Elshaht <elshahat799@gmail.com>
2025-03-03 13:48:02 +0200

    Time: 2282 ms (5.18%) | Memory: 41.8 MB (79.57%) - LeetSync

--- 494-target-sum/target-sum.cs
+++ 494-target-sum/target-sum.cs
@@ -1,10 +1,15 @@
 public class Solution {
+    Dictionary<(int,int) , int> cashe;
     public int FindTargetSumWays(int[] nums, int target) {
+        cashe = new Dictionary<(int,int),int>();
          return dfs(nums , target , 0 , 0);
     }
     private int dfs (int[] nums , int target , int i , int sum){
         if(i == nums.Length) return sum == target ? 1 : 0 ;
 
+        if(cashe.ContainsKey((i,sum))){
+            return cashe[(i,sum)];
+        }
         int add = dfs(nums , target  , i+1 , sum+nums[i]);
         int sub = dfs(nums , target , i+1  , sum-nums[i]);
         return add+sub;
